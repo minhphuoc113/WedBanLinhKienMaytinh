d4e34a2eb3afde7cd4c7a1a2c15df17b545f9dc2
vominhphuoc01012020@gmail.com <vominhphuoc01012020@gmail.com>
2021-11-18 21:09:20 +0700

    Ad

---
+++ WedBanLinhKienMayTinh/Areas/Admin/Controllers/HmController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WedBanLinhKienMayTinh.Areas.Admin.Controllers
+{
+    public class HmController : Controller
+    {
+        // GET: Admin/Hm
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
