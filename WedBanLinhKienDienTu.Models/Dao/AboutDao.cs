7ccb408e88eca4f30ee316be9ca56b0924190e14
Trung Nguyễn <trungdinhk27@gmail.com>
2021-12-20 19:08:08 +0700

    About

---
+++ WedBanLinhKienDienTu.Models/Dao/AboutDao.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WedBanLinhKienDienTu.Models.EF;
+
+namespace WedBanLinhKienDienTu.Models.Dao
+{
+    public class AboutDao
+    {
+        WebBanLinhKienMayTinhDbContext context;
+
+        public AboutDao()
+        {
+            context = new WebBanLinhKienMayTinhDbContext();
+        }
+        public IEnumerable<About> GetAbouts(long id)
+        {
+            SqlParameter[] param = new SqlParameter[]
+            {
+                 new SqlParameter("@ID", id)
+            };
+            var Reustl = context.Database.SqlQuery<About>("PSP_About_Select @ID", param).ToList();
+            return Reustl;
+        }
+
+
+
+        public About GetAboutByID(long id)
+        {
+            SqlParameter[] param = new SqlParameter[]
+            {
+                 new SqlParameter("@ID", id)
+            };
+            var Reustl = context.Database.SqlQuery<About>("PSP_About_Select @ID", param).SingleOrDefault();
+            return Reustl;
+        }
+        public int InsertAbout(About about)
+        {
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter("@ID", about.ID),
+                new SqlParameter("@Name", about.Name),
+                new SqlParameter("MetaTitle", about.MetaTitle),
+                new SqlParameter("@Description", about.Description),
+                new SqlParameter("@Image", about.Image),
+                new SqlParameter("@Detail", about.Detail),
+                new SqlParameter("@CreatedDate", about.CreatedDate),
+                new SqlParameter("@CreatedBy", about.CreatedBy),
+                new SqlParameter("@ModifiedDate", about.CreatedDate),
+                new SqlParameter("@ModifiedBy", about.CreatedBy),
+                new SqlParameter("@MetaKeywords", about.MetaKeywords),
+                new SqlParameter("@MetaDescriptions", about.MetaDescriptions),
+                new SqlParameter("@Status ", about.Status),
+            };
+            var reustl = context.Database.ExecuteSqlCommand("PSP_About_InsertAndUpdate @ID, @Name, @MetaTitle, @Description, @Image, @Detail, @CreatedDate, @CreatedBy, @ModifiedDate, @ModifiedBy, @MetaKeywords, @MetaDescriptions, @Status", param);
+            return reustl;
+        }
+        public int UpdateAbout(About about)
+        {
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter("@ID", about.ID),
+                new SqlParameter("@Name", about.Name),
+                new SqlParameter("MetaTitle", about.MetaTitle),
+                new SqlParameter("@Description", about.Description),
+                new SqlParameter("@Image", about.Image),
+                new SqlParameter("@Detail", about.Detail),
+                new SqlParameter("@CreatedDate", about.CreatedDate),
+                new SqlParameter("@CreatedBy", about.CreatedBy),
+                new SqlParameter("@ModifiedDate", about.CreatedDate),
+                new SqlParameter("@ModifiedBy", about.CreatedBy),
+                new SqlParameter("@MetaKeywords", about.MetaKeywords),
+                new SqlParameter("@MetaDescriptions", about.MetaDescriptions),
+                new SqlParameter("@Status ", about.Status),
+            };
+            var reustl = context.Database.ExecuteSqlCommand("PSP_About_InsertAndUpdate @ID, @Name, @MetaTitle, @Description, @Image, @Detail, @CreatedDate, @CreatedBy, @ModifiedDate, @ModifiedBy, @MetaKeywords, @MetaDescriptions, @Status", param);
+            return reustl;
+        }
+        public int Delete(About about)
+        {
+            SqlParameter[] param = new SqlParameter[]
+            {
+                 new SqlParameter("@ID", about.ID),
+            };
+            var reustl = context.Database.ExecuteSqlCommand("PSP_About_Delete @ID", param);
+            return reustl;
+        }
+
+    }
+}
---
+++ WedBanLinhKienMayTinh/Areas/Admin/Controllers/AboutController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WedBanLinhKienDienTu.Models.Dao;
+
+namespace WedBanLinhKienMayTinh.Areas.Admin.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: Admin/About
+        public ActionResult Index()
+        {
+            AboutDao aboutDao = new AboutDao();
+            var abouts = aboutDao.GetAbouts(0);
+            return View(abouts);
+        }
+
+        // GET: Admin/About/Details/5
+        public ActionResult Details(int id)
+        {
+            AboutDao aboutDao = new AboutDao();
+            var about = aboutDao.GetAboutByID(id);
+            return View(about);
+        }
+
+        // GET: Admin/About/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/About/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/About/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/About/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/About/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/About/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
