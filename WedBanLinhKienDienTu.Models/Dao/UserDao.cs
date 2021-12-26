ac6dfb980b6d7cd999afad97c1d0ac7f5381f576
vominhphuoc01012020@gmail.com <vominhphuoc01012020@gmail.com>
2021-12-26 11:10:56 +0700

    User

--- WedBanLinhKienDienTu.Models/Dao/UserDao.cs
+++ WedBanLinhKienDienTu.Models/Dao/UserDao.cs
@@ -1,12 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WedBanLinhKienDienTu.Models.EF;
 
 namespace WedBanLinhKienDienTu.Models.Dao
 {
     public class UserDao
     {
+        WebBanLinhKienMayTinhDbContext context;
+
+        public UserDao()
+        {
+            context = new WebBanLinhKienMayTinhDbContext();
+        }
+        public IEnumerable<User> GetUsers(long id)
+        {
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter ("@ID", id),
+            };
+            var result = context.Database.SqlQuery<User>("PSP_User_Select @ID", param).ToList();
+            return result;
+        }
+        public User GetUserById(long id)
+        {
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter ("@ID", id),
+            };
+            var result = context.Database.SqlQuery<User>("PSP_User_Select @ID", param).SingleOrDefault();
+            return result;
+        }
+        public int InsertUser(User user)
+        {
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter ("@ID", user.ID),
+                 new SqlParameter ("@UserName", user.UserName),
+                  new SqlParameter ("@Password", user.Password),
+                   new SqlParameter ("@GroupID", user.GroupID),
+                    new SqlParameter ("@Name", user.Name),
+                     new SqlParameter ("@Email", user.Email),
+                      new SqlParameter ("@Phone", user.Phone),
+                       new SqlParameter ("@Status", user.Status),
+            };
+            var result = context.Database.ExecuteSqlCommand("PSP_User_InsertAndUpdate @ID,@UserName,@Password,@GroupID,@Name,@Email,@Phone,@Status", param);
+            return result;
+        }
+        public int UpdateUser(User user)
+        {
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter ("@ID", user.ID),
+                 new SqlParameter ("@UserName", user.UserName),
+                  new SqlParameter ("@Password", user.Password),
+                   new SqlParameter ("@GroupID", user.GroupID),
+                    new SqlParameter ("@Name", user.Name),
+                     new SqlParameter ("@Email", user.Email),
+                      new SqlParameter ("@Phone", user.Phone),
+                       new SqlParameter ("@Status", user.Status),
+            };
+            var result = context.Database.ExecuteSqlCommand("PSP_User_InsertAndUpdate @ID,@UserName,@Password,@GroupID,@Name,@Email,@Phone,@Status", param);
+            return result;
+        }
+        public int DeleteUser(User user)
+        {
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter ("@ID", user.ID),
+
+            };
+            var result = context.Database.ExecuteSqlCommand("PSP_User_Delete @ID", param);
+            return result;
+        }
     }
 }
---
+++ WedBanLinhKienMayTinh/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WedBanLinhKienDienTu.Models.Dao;
+
+namespace WedBanLinhKienMayTinh.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Admin/User
+        public ActionResult Index()
+        {
+            UserDao userDao = new UserDao();
+            var users = userDao.GetUsers(0);
+            return View(users);
+        }
+
+        // GET: Admin/User/Details/5
+        public ActionResult Details(int id)
+        {
+            UserDao userDao = new UserDao();
+            var user = userDao.GetUserById(id);
+            return View(user);
+        }
+
+        // GET: Admin/User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/User/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            UserDao userDao = new UserDao();
+            var user = userDao.GetUserById(id);
+            return View(user);
+
+        }
+
+        // POST: Admin/User/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            UserDao userDao = new UserDao();
+            var user = userDao.GetUserById(id);
+            return View();
+        }
+
+        // POST: Admin/User/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
