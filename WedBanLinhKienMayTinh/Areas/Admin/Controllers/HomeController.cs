ce2bc103ad39c80bbf7128aa7732c93f84b7299a
vominhphuoc01012020@gmail.com <vominhphuoc01012020@gmail.com>
2022-01-04 15:01:51 +0700

    Làm xong Login

--- WedBanLinhKienMayTinh/Areas/Admin/Controllers/HomeController.cs
+++ WedBanLinhKienMayTinh/Areas/Admin/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WedBanLinhKienDienTu.Models.EF;
 
 namespace WedBanLinhKienMayTinh.Areas.Admin.Controllers
 {
