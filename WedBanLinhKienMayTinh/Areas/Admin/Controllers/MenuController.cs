ed79cca925bb103620dbcad50c045b1daab56724
vominhphuoc01012020@gmail.com <vominhphuoc01012020@gmail.com>
2022-01-13 22:21:46 +0700

    Menu

--- WedBanLinhKienMayTinh/Areas/Admin/Controllers/MenuController.cs
+++ WedBanLinhKienMayTinh/Areas/Admin/Controllers/MenuController.cs
@@ -1,94 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using WedBanLinhKienDienTu.Models.Dao;
+using WedBanLinhKienDienTu.Models.EF;
+using WedBanLinhKienMayTinh.Library;
 
 namespace WedBanLinhKienMayTinh.Areas.Admin.Controllers
 {
     public class MenuController : Controller
     {
+        CategoryDao categoryDao = new CategoryDao();
+        AboutDao aboutDao = new AboutDao();
+        ContentDao contentDao = new ContentDao();
+        MenuDao menuDao = new MenuDao();
         // GET: Admin/Menu
         public ActionResult Index()
         {
-            MenuDao menuDao = new MenuDao();
-            var menues = menuDao.GetMenus(0);
-            return View(menues);
+            ViewBag.ListCategory = categoryDao.getList();
+            List<Menu> menu = menuDao.getList();
+            return View("Index", menu);
         }
-
-        // GET: Admin/Menu/Details/5
-        public ActionResult Details(int id)
-        {
-            MenuDao menuDao = new MenuDao();
-            var menu = menuDao.GetMenuByID(id);
-            return View(menu);
-        }
-
-        // GET: Admin/Menu/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Admin/Menu/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Index(FormCollection form)
         {
-            try
+            if (!string.IsNullOrEmpty(form["ThemCategory"]))
             {
-                // TODO: Add insert logic here
+                if (!string.IsNullOrEmpty(form["itemcat"]))
+                {
+                    var listitem = form["itemcat"];
+                    var listarr = listitem.Split(',');
+                    foreach (var row in listarr)
+                    {
+                        int id = int.Parse(row);
+                        Category category = categoryDao.getRow(id);
+                        Menu menu = new Menu();
+                        menu.Text = category.Name;
 
-                return RedirectToAction("Index");
+                    }
+                }
+                else
+                {
+                    TempData["message"] = new XMessage("danger", "Chưa chọn danh mục sản phẩm");
+                }
             }
-            catch
-            {
-                return View();
-            }
-        }
 
-        // GET: Admin/Menu/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Admin/Menu/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Admin/Menu/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Admin/Menu/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            return RedirectToAction("Index", "Menu");
         }
     }
 }
